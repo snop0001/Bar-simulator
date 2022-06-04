201a70cd5b60bb3a53882d91c3c2466953aa6750
Shirel Snopik <snop0001@hz.nl>
2022-06-04 22:42:48 +0200

    editing register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\Console\Input\Input;
@@ -48,13 +49,21 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data)
+    protected function validator(Request $request)
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'max:255', 'unique:users','email'],
-            'password' => ['required', 'string', 'min:8','confirmed'],
+        $validatedAtributes = $request->validate([
+            'name' => ['required'| 'string'| 'max:255'],
+            'email' => ['required'| 'string'| 'max:255'|'unique:users'|'email'],
+            'password' => ['required'|'string'|'min:8'|'confirmed'],
+
         ]);
+
+        return $validatedAtributes;
+//        return Validator::make($request, [
+//            'name' => ['required', 'string', 'max:255'],
+//            'email' => ['required', 'string', 'max:255', 'unique:users','email'],
+//            'password' => ['required', 'string', 'min:8','confirmed'],
+//        ]);
     }
 
     /**
@@ -63,9 +72,10 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(array $data)
+    protected function create(Request $request)
     {
-        return User::create($this->validator($data));
-        
+
+        return User::create($this->validator($request));
+
     }
 }
