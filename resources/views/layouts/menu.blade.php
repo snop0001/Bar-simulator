603399e7c00ebac5b23a1bb4d64b0adf25324766
Shirel Snopik <snop0001@hz.nl>
2022-06-05 01:19:46 +0200

    testing that everything works

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -8,4 +8,11 @@
         <i class="nav-icon fas fa-tags"></i>
         <p>Orders</p>
     </a>
+    <a href="#" class="btn btn-default btn-flat float-right"
+       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        Sign out
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
 </li>
