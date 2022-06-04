70cff33690feaa904a251dcc5f1fd28e157662f1
Shirel Snopik <snop0001@hz.nl>
2022-06-04 12:17:59 +0200

    create edit

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -112,11 +112,11 @@
                             <label for="allergies">Do you have allergies?</label>
                                 <div>
                                     <div class="custom-control custom-radio custom-control-inline">
-                                        <input onclick=isChecked() type="radio" id="al1" name="allergies" class="custom-control-input" value=1 {{$order->allergies===1 ? 'checked='.'"'.'checked'.'"' : '' }} required>
+                                        <input onclick=isChecked() type="radio" id="al1" name="allergies" class="custom-control-input" value=1 {{$order->allergies===true ? 'checked='.'"'.'checked'.'"' : '' }} required>
                                         <label class="custom-control-label" for="al1">Yes</label>
                                     </div>
                                     <div class="custom-control custom-radio custom-control-inline">
-                                        <input onclick=isChecked() type="radio" id="al2" name="allergies" class="custom-control-input" value=0 {{$order->allergies===0 ? 'checked='.'"'.'checked'.'"' : '' }}required>
+                                        <input onclick=isChecked() type="radio" id="al2" name="allergies" class="custom-control-input" value=0 {{$order->allergies===false ? 'checked='.'"'.'checked'.'"' : '' }}required>
                                         <label class="custom-control-label" for="al2">No</label>
                                     </div>
                                 </div>
