e12fc70304cd25a4d6189e27211f43d8acd6c4c8
Shirel Snopik <snop0001@hz.nl>
2022-06-04 22:35:44 +0200

    editing register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,10 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        return User::create($this->validator($data));
+        
     }
 }
