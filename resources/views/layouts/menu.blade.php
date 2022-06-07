be7a4fbc6c15118ebb3e496180a398c3d0fa4ce3
Shirel Snopik <snop0001@hz.nl>
2022-06-07 10:47:10 +0200

    starting fresh

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,4 +4,8 @@
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
+    <a href="{{route('orders.index') }}" class="nav-link {{ Request::is('orders') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-tags"></i>
+        <p>Orders</p>
+    </a>
 </li>
