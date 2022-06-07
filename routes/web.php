8292caac20e0137a113d22a1b1307b7659a0f4b3
Shirel Snopik <snop0001@hz.nl>
2022-06-07 09:58:18 +0200

    try to fix errors registering

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,7 @@ Auth::routes();
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
